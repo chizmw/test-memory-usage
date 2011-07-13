b63bd7e0a123b66c59f25df6b4a08a876dce26ad
Chisel <chisel@chizography.net>
2011-07-13 22:08:40 +0100

    Initial commit

---
+++ lib/Test/Memory/Usage.pm
@@ -0,0 +1,14 @@
+package Test::Memory::Usage;
+# ABSTRACT: Test::Memory::Usage needs a more meaningful abstract
+use strict;
+use warnings;
+
+=head1 NAME
+
+Test::Memory::Usage
+
+=cut
+ 
+1;
+__END__
+# vim: ts=8 sts=4 et sw=4 sr sta
